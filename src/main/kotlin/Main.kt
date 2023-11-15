c5f3f79a5ba5739a597dc04ee7231158547b9dfc
Mikaelson Macena <mikaelson.macena@lecom.com.br>
2023-11-15 11:35:09 -0300

    Conhecendo os tipos de variaveis numéricos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,8 @@
 fun main() {
-    println("Hello World")
+    println("Valor mínimo de um Int: ${Int.MIN_VALUE}, Valor máximo: ${Int.MAX_VALUE}");
+    println("Valor mínimo de um Long: ${Long.MIN_VALUE}, Valor máximo: ${Long.MAX_VALUE}");
+    println("Valor mínimo de um Double: ${Double.MIN_VALUE}, Valor máximo: ${Double.MAX_VALUE}");
+    println("Valor mínimo de um Float: ${Float.MIN_VALUE}, Valor máximo: ${Float.MAX_VALUE}");
+    println("Valor mínimo de um Short: ${Short.MIN_VALUE}, Valor máximo: ${Short.MAX_VALUE}");
+    println("Valor mínimo de um Byte: ${Byte.MIN_VALUE}, Valor máximo: ${Byte.MAX_VALUE}");
 }
